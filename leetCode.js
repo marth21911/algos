839ee599ed8b29977b218d2113db8daa4971a38e
Brian Marth <marthbrian@gmail.com>
2022-05-16 17:03:30 -0400

    test

--- leetCode.js
+++ leetCode.js
@@ -38,27 +38,69 @@
 
 //Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-function maxSubArray(nums){
-    var maxSum=null;
-    var curSum=null;
-    for(var i=0;i<nums.length;i++){
-        if(nums[i]>(nums[i]+curSum)){
-            console.log(curSum);
-            curSum=nums[i];
-            console.log(curSum);
-        }else{
+// function maxSubArray(nums){
+//     var maxSum=null;
+//     var curSum=null;
+//     for(var i=0;i<nums.length;i++){
+//         if(nums[i]>(nums[i]+curSum)){
+//             console.log(curSum);
+//             curSum=nums[i];
+//             console.log(curSum);
+//         }else{
             
-            curSum+=nums[i];
-            console.log(curSum)
-            console.log(maxSum)
-        }
-        if(maxSum==null){
-            maxSum= curSum;
-        }else{
-            maxSum=Math.max(curSum,maxSum);
+//             curSum+=nums[i];
+//             console.log(curSum)
+//             console.log(maxSum)
+//         }
+//         if(maxSum==null){
+//             maxSum= curSum;
+//         }else{
+//             maxSum=Math.max(curSum,maxSum);
+//         }
+//     }
+//     return maxSum;
+// }
+// var nums = [-1];
+// console.log(maxSubArray(nums));
+
+
+function twosum(nums,target){
+    var answer=[];
+    for(var i = 0; i<nums.length;i++){
+        for(j=(i+1);j<nums.length;j++){
+            if((nums[i] + nums[j])==target){
+                answer.push(i);
+                answer.push(j);
+            }
         }
     }
-    return maxSum;
+    return answer
 }
-var nums = [-1];
-console.log(maxSubArray(nums));
+
+function merge(nums1, m, nums2, n){
+    var newArr = [];
+    for(var i=0; i<(m+n);i++){
+        console.log(i);
+        console.log("Comparing " + nums1[0] + " and " + nums2[0]);
+        if(nums1[0]<=nums2[0] && nums1[0]!=0){
+            console.log("if")
+            newArr.push(nums1[0]);
+            nums1.shift();
+            
+        }else{
+            console.log("else")
+            newArr.push(nums2[0]);
+            nums2.shift();
+        }
+    }
+    nums1= newArr
+    console.log(nums1);
+    return nums1
+}
+
+
+var x=[1,2,3,0,0,0]
+var y= 3
+var z= [2,5,6]
+var c= 3
+console.log(merge(x,y,z,c));
